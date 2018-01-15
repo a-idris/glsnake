5fa4b80b95ff1b29066bd041838af6cc6f8936ad
a-idris <a.idris.1@warwick.ac.uk>
2018-01-15 02:49:33 +0000

    add shading, lighting, camera controls and perspective switch

--- snake.cpp
+++ snake.cpp
@@ -13,6 +13,19 @@
 //for head, direction = arrow direction (last changed)
 //for else, direction = prev direction of next block
 
+// class Snake {
+// private:
+// 	SnakeBlock head;
+// 	int length, score;
+// }
+
+// class SnakeBlock {
+	
+// 	public:
+// 		int xpos, ypos;
+// 		//vector direction;
+// 		SnakeBlock getNextSnakeBlock();
+// }
 
 /*game: grid, snake.
 
@@ -20,12 +33,44 @@
 
 //decls:
 void draw_border(int, int);
+void init_lights();
 void orthographic_vv();
 void perspective_vv();
 
 const int grid_size = 20;
 
-unsigned int g_grid = 0; 
+unsigned int g_grid = 0;
+
+float camera_delta = 0.5f;
+float camera_xoffset = 0.0f;
+float camera_zoffset = grid_size / 2.0f;
+
+enum perspective_t {ORTHOGRAPHIC=0, PERSPECTIVE=1, PERSPECTIVES_COUNT = 2};
+perspective_t current_perspective = PERSPECTIVE;
+
+struct material_t {
+	float ambient[4];
+	float diffuse[4];
+	float specular[4];
+	float shininess;
+};
+
+//snake material
+const material_t snake_mat = {
+	{0.05f, 0.4f, 0.05f, 1.0f},
+	{0.0f, 0.6f, 0.0f, 1.0f}, 
+	{0.6f, 0.8f, 0.6f, 1.0f},
+	30.0f
+};
+
+//food material 
+
+void set_material(const material_t& mat) {
+	glMaterialfv(GL_FRONT, GL_AMBIENT, mat.ambient);
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, mat.diffuse);
+	glMaterialfv(GL_FRONT, GL_SPECULAR, mat.specular);
+	glMaterialf(GL_FRONT, GL_SHININESS, mat.shininess);
+}
 
 void display()
 {
@@ -33,18 +78,55 @@ void display()
 
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-	// gluLookAt(0, 3, 0,
-	// 		  0, 0, 0,
-	// 		  0, 0, -1);
 
-	gluLookAt(0, grid_size, grid_size / 2.0f,
-			  0, 0, 0,
-			  0, 1, 0);
+	if (current_perspective == ORTHOGRAPHIC) {
+		gluLookAt(0, 3, 0,
+				  0, 0, 0,
+				  0, 0, -1);		
+	} else if (current_perspective == PERSPECTIVE) {
+		gluLookAt(camera_xoffset, grid_size, camera_zoffset,
+				  0, 0, 0,
+				  0, 1, 0);
+	}
+	//if pov, camera pos = snake head, look direction = head + direction
 
 	//animate game state. vars for xpos, ypos, etc.
+
+	glDisable(GL_LIGHTING);
+	glColor3f(1.0f, 1.0f, 1.0f);
 	glCallList(g_grid);
+	glEnable(GL_LIGHTING);
+
+	set_material(snake_mat);
+
+	glPushMatrix();
+	glTranslatef(0.5f, 0.5f, 0.5f);
+
 
 	glutSolidCube(1);
+
+	glPushMatrix();
+	glTranslatef(5.0f, 0.0f, 1.0f);
+	glutSolidCube(1);
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(5.0f, 0.0f, 1.0f);
+	glutSolidCube(1);
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(9.0f, 0.0f, 9.0f);
+	glutSolidCube(1);
+	glPopMatrix();
+
+	glPushMatrix();
+	glTranslatef(9.0f, 0.0f, -9.0f);
+	glutSolidCube(1);
+	glPopMatrix();
+
+
+	glPopMatrix();
 
 	glutSwapBuffers(); 
 }
@@ -89,10 +171,6 @@ void draw_border(int w, int h) {
 
 }
 
-void draw_cube() {
-
-}
-
 
 bool check_alive() {
 	//collided with itself
@@ -110,6 +188,9 @@ void init()
 	g_grid = make_grid(grid_size, grid_size);
 
 	perspective_vv();
+	init_lights();
+	glEnable(GL_DEPTH_TEST);
+	glShadeModel(GL_FLAT);
 }
 
 void orthographic_vv() {
@@ -124,6 +205,21 @@ void perspective_vv() {
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	gluPerspective(60.0f, 1.0f, grid_size * 0.4f, grid_size * 2.0f);
+}
+
+void init_lights() {
+	float light_ambient[] = {0.1, 0.1, 0.1, 1.0};
+	float light_diffuse[] = {0.5, 0.5, 0.5, 1.0};
+	float light_specular[] = {1.0f, 1.0f, 1.0f, 1.0f};
+	glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+	glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+	// fix the light above the centre of the grid
+	float light_position[] = {0.0, grid_size * 0.9, 0.0, 0.0};
+	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+	// enable lighting and turn on the light0
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
 }
 
 // our idle handler
@@ -153,7 +249,11 @@ void visibility(int vis)
 void reshape(int w, int h) {
 	glViewport(0, 0, w, h);
 	std::cerr << "reshape" << std::endl;
-	perspective_vv();
+	if (current_perspective == ORTHOGRAPHIC) {
+		orthographic_vv();
+	} else {
+		perspective_vv();
+	}
 }
 
 // will get which key was pressed and x and y positions if required
@@ -168,16 +268,22 @@ void keyboard(unsigned char key, int, int)
 		//WASD for camera movement?
 
 		case 'w': 
-			// vector = (0, 1); break;
+			camera_zoffset -= camera_delta; break;
 		case 'a':
-			// vector = (-1, 0); break;
+			camera_xoffset -= camera_delta; break;
 		case 's': 
-			// vector = (0, -1); break;
+			camera_zoffset += camera_delta; break;
 		case 'd':
-			// vector = (1, 0); break;
+			camera_xoffset += camera_delta; break;
 		case 'p':
-			//switch perspective (change camera vars)
-		break;
+			//cycle through perspectives 
+			current_perspective = static_cast<perspective_t>((current_perspective + 1) % PERSPECTIVES_COUNT);
+			if (current_perspective == ORTHOGRAPHIC) {
+				orthographic_vv();
+			} else {
+				perspective_vv();
+			}
+			break;
 	}
 
 	glutPostRedisplay(); // force a redraw
